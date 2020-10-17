71a8e1cbcdefe67316a9e87da756eb84354809dd
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2020-10-17 20:45:58 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+<html>
+<head>
+</head>
+<body>
+<h1  align="center">Read the below to know how to use check the points table</h1>
+
+<ul>
+<li>visit <a href="https://iplpoints.herokuapp.com/ipl.php"> this link</a></li>
+<li>Pass the parameter in the following manner <code>https://iplpoints.herokuapp.com/ipl.php?year=2018</code>
+where we need to pass the parameter " ?year " and followed by the year of which you want
+<li>There you have the ipl points for the following  year</li>
+</ul>
+</body>
+</html>
+?>
